142e6165bd552c2218ac06213b8575450e1484a6
426f776965 <67214185+426f776965@users.noreply.github.com>
2020-07-15 03:21:24 -0400

    Create dllmain.cpp

---
+++ ep6/item mall/source/dllmain.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "database.h"
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved)
+{
+	switch (dwReason)
+	{
+	case DLL_PROCESS_ATTACH:
+		CreateThread(NULL, 0, ThreadProc1, 0, NULL, 0);
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
