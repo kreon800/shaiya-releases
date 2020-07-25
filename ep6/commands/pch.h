1677cf8803ccb83bc144ea85a155a07908e3123b
426f776965 <67214185+426f776965@users.noreply.github.com>
2020-07-25 01:28:15 -0400

    Create pch.h

---
+++ ep6/commands/pch.h
@@ -0,0 +1,21 @@
+#ifndef PCH_H
+#define PCH_H
+
+#define WIN32_LEAN_AND_MEAN 
+#include <windows.h>
+//main function
+void cmdFunc();
+//hook function
+BOOL Hook(void * pAddr, void * pNAddr, int len = 5);
+//variables and game.exe addresses
+DWORD cmdChk = 0x632D07;
+DWORD cmdRet = 0x4867A6;
+DWORD cmdJMP = 0x487532;
+LPCSTR szClose = "/close";
+LPCSTR szMarket = "/market";
+LPCSTR szKeeper = "/warehouse";
+LPCSTR szRepair = "/repair";
+LPCSTR szReroll = "/recreate";
+unsigned char stack[] = { 0x90, 0x90 };
+
+#endif
