60ead029460f0796746073d13dd9dd677b22c12e
426f776965 <67214185+426f776965@users.noreply.github.com>
2020-08-20 15:19:37 -0400

    Create pch.cpp

---
+++ ep5/ps_game/source/pch.cpp
@@ -0,0 +1,10 @@
+#include "pch.h"
+
+void ps_game() {
+	//names are case-insensitive
+	LoadLibraryA("shaiyaeden");
+	LoadLibraryA("123123");
+	LoadLibraryA("skill");
+	LoadLibraryA("exp");
+	LoadLibraryA("sharekills");
+}
